3808f25bc99d2aee5d3943cbf4cfb074f7567aae
Alan de Oliveira Gonçalves <alandeog@poli.ufrj.br>
2025-10-28 23:16:39 -0300

    Add SQL query to calculate total order value

---
+++ modulo-6/query-3.sql
@@ -0,0 +1,19 @@
+SELECT
+    c.codigo_comanda,
+    c.data_comanda,
+    c.numero_mesa,
+    c.nome_cliente,
+    SUM(ic.quantidade * ca.preco_unitario) AS valor_total_comanda
+FROM
+    Comanda c
+JOIN
+    Itens_Comanda ic ON c.codigo_comanda = ic.codigo_comanda
+JOIN
+    Cardapio ca ON ic.codigo_cardapio = ca.codigo_cardapio
+GROUP BY
+    c.codigo_comanda,
+    c.data_comanda,
+    c.numero_mesa,
+    c.nome_cliente
+ORDER BY
+    c.data_comanda;
